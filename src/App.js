e5ff5a806663a976c67af8508b740c28715c557a
lucievr <lucy.vrsovska@gmail.com>
2020-10-28 13:01:02 +0100

    Create a header with logo and basic sign in form

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/signin-and-signup/signin-and-signup.component';
+import Header from './components/header/header.component';
 
 const App = () => (
     <div>
+    <Header />
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
     </Switch>
     </div>
 );
