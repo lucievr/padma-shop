43b202563a00804e498aaa2dce9b79b526a3ec73
lucievr <lucy.vrsovska@gmail.com>
2020-10-29 15:00:20 +0100

    Create collections overview and routing using param id

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,25 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
+
+const COLLECTION_ID_MAP = {
+  soaps: 1,
+  'essential-oils': 2,
+  'bath-salts': 3,
+  'bathroom-essentials': 4,
+  'covid-essentials': 5,
+};
 
 const selectShop = (state) => state.shop;
 
 export const selectShopCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+export const selectCollection = memoize((collectionUrlParam) =>
+  createSelector([selectShopCollections], (collections) =>
+    collections.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  )
 );
