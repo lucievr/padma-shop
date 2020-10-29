94d09f48c19f1d83092be8712d903e1a92bafe4e
lucievr <lucy.vrsovska@gmail.com>
2020-10-29 18:20:15 +0100

    Normalise shop data and complete collection page

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -5,21 +5,21 @@ const INITIAL_STATE = {
       imageUrl:
         'https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/padma-shop%2Fsoaps.jpg?alt=media&token=6140b175-6946-4899-a21d-d4fbfc421b1f',
       id: 1,
-      linkUrl: 'soaps',
+      linkUrl: 'shop/soaps',
     },
     {
       title: 'Essential oils',
       imageUrl:
         'https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/padma-shop%2Fessential-oils.jpg?alt=media&token=960a8128-8a80-48ee-b76b-80ae19b39a54',
       id: 2,
-      linkUrl: '',
+      linkUrl: 'shop/essential-oils',
     },
     {
       title: 'Bath salts',
       imageUrl:
         'https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/padma-shop%2Fbath-salts.jpg?alt=media&token=bac7065c-7bfa-4685-b160-b28328cb10f6',
       id: 3,
-      linkUrl: '',
+      linkUrl: 'shop/bath-salts',
     },
     {
       title: 'Bathroom essentials',
@@ -27,7 +27,7 @@ const INITIAL_STATE = {
         'https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/padma-shop%2Fbathroom-essentials.jpg?alt=media&token=b2ceef8e-5722-4bce-9d3d-a3b6c0cf446b',
       size: 'large',
       id: 4,
-      linkUrl: '',
+      linkUrl: 'shop/bathroom-essentials',
     },
     {
       title: 'COVID essentials',
@@ -35,7 +35,7 @@ const INITIAL_STATE = {
         'https://firebasestorage.googleapis.com/v0/b/shoolix.appspot.com/o/padma-shop%2Fcovid-essentials.jpg?alt=media&token=d34b222e-de05-4b10-aa53-ee642d79c1a1',
       size: 'large',
       id: 5,
-      linkUrl: '',
+      linkUrl: 'shop/covid-essentials',
     },
   ],
 };
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,6 @@
 import { createSelector } from 'reselect';
 import memoize from 'lodash.memoize';
 
-const COLLECTION_ID_MAP = {
-  soaps: 1,
-  'essential-oils': 2,
-  'bath-salts': 3,
-  'bathroom-essentials': 4,
-  'covid-essentials': 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectShopCollections = createSelector(
@@ -16,10 +8,14 @@ export const selectShopCollections = createSelector(
   (shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  collections => Object.keys(collections).map((key) => collections[key])
+)
+
 export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector([selectShopCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectShopCollections],
+    (collections) => collections[collectionUrlParam]
   )
 );
