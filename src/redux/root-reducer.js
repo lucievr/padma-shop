38f973805d1c18e4e3b1e7df3b5d88cea3628894
lucievr <lucy.vrsovska@gmail.com>
2020-10-29 12:34:27 +0100

    Add redux persist and create persisted store and reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,19 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // local storage
 
 import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer'
+import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'], // no need to add user, that is handled and persisted by firebase
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
