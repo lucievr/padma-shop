55755bbdbfc7b71b87bd7ddcd4cbe37cc3546817
lucievr <lucy.vrsovska@gmail.com>
2020-10-31 23:17:37 +0100

    Add media queries for responsivity and change url links for directory images

--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -3,28 +3,28 @@ const INITIAL_STATE = {
     {
       title: 'Soaps',
       imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/soaps.jpg?alt=media&token=dda26f93-e888-4feb-99bc-6913c9819318',
+        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/soaps.jpg?alt=media&token=634d3d6b-c1f0-4cf3-ae68-6a45e4c4d59d',
       id: 1,
       linkUrl: 'shop/soaps',
     },
     {
       title: 'Essential oils',
       imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/essential-oils.jpg?alt=media&token=3b77a7d6-c3eb-4bbd-8a5a-3fae09dab70c',
+        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/essential-oils.jpg?alt=media&token=a96f0e13-57b2-4985-9976-e589abd3be0b',
       id: 2,
       linkUrl: 'shop/essential-oils',
     },
     {
       title: 'Bath salts',
       imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/bath-salts.jpg?alt=media&token=9a406f54-ac58-42db-81a3-16cdd4fab983',
+        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/bath-salts.jpg?alt=media&token=2780a577-4336-4158-9bb5-827cbaabe457',
       id: 3,
       linkUrl: 'shop/bath-salts',
     },
     {
       title: 'Bathroom essentials',
       imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/bathroom-essentials.jpg?alt=media&token=0ed6ff84-2db6-44ae-9256-d986ee6ba11a',
+        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/bathroom-essentials.jpg?alt=media&token=990786b5-7ce7-463f-b33b-cc03d3fb80e8',
       size: 'large',
       id: 4,
       linkUrl: 'shop/bathroom-essentials',
@@ -32,7 +32,7 @@ const INITIAL_STATE = {
     {
       title: 'COVID essentials',
       imageUrl:
-        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/covid-essentials.jpg?alt=media&token=754478b5-8cb3-4368-a34f-fb836348dfb3',
+        'https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/covid-essentials.jpg?alt=media&token=e4360c96-e0da-48ca-9eef-888072396fb5',
       size: 'large',
       id: 5,
       linkUrl: 'shop/covid-essentials',
