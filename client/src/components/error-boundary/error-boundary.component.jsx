99f8442fe390be1999dc5540f7512ef55c1fef80
lucievr <lucy.vrsovska@gmail.com>
2020-11-01 00:29:37 +0100

    Add react lazy loading and suspense plus handle error boundaries

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import './error-boundary.styles.scss'
+
+class ErrorBoundary extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasErrored: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    console.log(error, 'error');
+    return { hasErrored: true };
+  }
+
+  render() {
+    if (this.state.hasErrored) {
+      return (
+        <div className='error-image-overlay'>
+          <div
+            className='error-image-container'
+            style={{
+              backgroundImage: 'url(https://i.imgur.com/flHudHE.png)',
+            }}
+          ></div>
+          <h2 className='error-image-text'>Sorry, this page is lost...</h2>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
