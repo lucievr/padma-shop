fada734970680b51fa83fb71c78f01c2ccb4ef31
lucievr <lucy.vrsovska@gmail.com>
2020-11-05 21:21:49 +0100

    Add subtotal and shipping cost to checkout overview

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -19,55 +19,70 @@ import { ReactComponent as Bin } from '../../assets/bin.svg';
 
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total, clearCart }) => (
-  <div className='checkout-page'>
-    <div className='checkout-header'>
-      <div className='header-block product' />
-      <div className='header-block name'>
-        <span>Product</span>
+const CheckoutPage = ({ cartItems, total, clearCart }) => {
+  let finalTotal = Number(total);
+
+  if (Number(total) < 49) {
+    finalTotal = finalTotal + 4.95;
+  }
+
+  return (
+    <div className='checkout-page'>
+      <div className='offer-msg'>FREE DELIVERY ON ALL ORDERS OVER € 49!</div>
+      <div className='checkout-header'>
+        <div className='header-block product' />
+        <div className='header-block name'>
+          <span>Product</span>
+        </div>
+        <div className='header-block quantity'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block price'>
+          <span>Price</span>
+        </div>
+        <div className='header-block' />
       </div>
-      <div className='header-block quantity'>
-        <span>Quantity</span>
-      </div>
-      <div className='header-block price'>
-        <span>Price</span>
-      </div>
-      <div className='header-block' />
-    </div>
-    {cartItems.map((cartItem) => (
-      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-    ))}
-    {cartItems.length > 0 ? (
-      <>
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+      ))}
+      {cartItems.length > 0 ? (
+        <>
+          <div className='checkout-footer'>
+            <span className='clear-cart' onClick={clearCart}>
+              <Bin className='bin' />
+              &nbsp;Clear cart
+            </span>
+            <div className='total-price'>
+              <span className='subtotal'>Subtotal: € {total}</span>
+              <span className='shipping'>
+                Shipping:{' '}
+                {total < 49 ? '€ 4.95' : <span className='free'>FREE</span>}
+              </span>
+              <span className='total'>TOTAL: € {finalTotal.toFixed(2)}</span>
+            </div>
+          </div>
+          <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242
+          </div>
+          <StripeCheckoutButton price={finalTotal} />
+          <div className='credit-cards'>
+            <Mastercard />
+            <Visa />
+            <AmericanExpress />
+            <Jcb />
+            <Discover />
+          </div>
+        </>
+      ) : (
         <div className='checkout-footer'>
-          <span className='clear-cart' onClick={clearCart}>
-            <Bin className='bin' />
-            &nbsp;Clear cart
-          </span>
-          <span className='total'>TOTAL: € {total}</span>
+          <div className='total'>TOTAL: € {total}</div>
         </div>
-        <div className='test-warning'>
-          *Please use the following test credit card for payments*
-          <br />
-          4242 4242 4242 4242
-        </div>
-        <StripeCheckoutButton price={total} />
-        <div className='credit-cards'>
-          <Mastercard />
-          <Visa />
-          <AmericanExpress />
-          <Jcb />
-          <Discover />
-        </div>
-      </>
-    ) : (
-      <div className='checkout-footer'>
-        <span />
-        <span className='total'>TOTAL: € {total}</span>
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
