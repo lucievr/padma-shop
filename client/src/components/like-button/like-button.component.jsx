8848b3813bfea21ca6dec6f288358bba30ce9d8a
lucievr <lucy.vrsovska@gmail.com>
2020-11-05 19:51:12 +0100

    Add custom alerts and clear cart after successful purchase

--- client/src/components/like-button/like-button.component.jsx
+++ client/src/components/like-button/like-button.component.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import Tooltip from '@material-ui/core/Tooltip';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
 
 import {
   selectCurrentUser,
   selectUserFavourites,
 } from '../../redux/user/user.selectors.js';
-import { addToFavourites, removeFromFavourites } from '../../redux/user/user.actions';
+import {
+  addToFavourites,
+  removeFromFavourites,
+} from '../../redux/user/user.actions';
 
 import { ReactComponent as HeartOutlined } from '../../assets/heart-outlined.svg';
 import { ReactComponent as HeartFilled } from '../../assets/heart-filled.svg';
@@ -20,35 +25,51 @@ const LikeButton = ({
   addToFavourites,
   removeFromFavourites,
 }) => {
-  const handleAddToFavourites = () => {
-    if (currentUser) {
-      addToFavourites(item.id);
-    } else {
-      alert('You need to be signed in to add items to favourites');
+  const [alertOpen, setAlertOpen] = useState(false);
+
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') {
+      return;
     }
+    setAlertOpen(false);
   };
+
+  const handleAddToFavourites = () =>
+    currentUser ? addToFavourites(item.id) : setAlertOpen(true);
 
   const handleRemoveFromFavourites = () => {
     if (!currentUser) return;
     removeFromFavourites(item.id);
   };
-  
-  return currentUser &&
-    userFavourites &&
-    userFavourites.includes(item.id) ? (
-    <Tooltip title='Remove from favourites' placement='top'>
-      <HeartFilled
-        className='heart-filled'
-        onClick={handleRemoveFromFavourites}
-      />
-    </Tooltip>
-  ) : (
-    <Tooltip title='Add to favourites' placement='top'>
-      <HeartOutlined
-        className='heart-outlined'
-        onClick={handleAddToFavourites}
-      />
-    </Tooltip>
+
+  return (
+    <>
+      {currentUser && userFavourites && userFavourites.includes(item.id) ? (
+        <Tooltip title='Remove from favourites' placement='top'>
+          <HeartFilled
+            className='heart-filled'
+            onClick={handleRemoveFromFavourites}
+          />
+        </Tooltip>
+      ) : (
+        <Tooltip title='Add to favourites' placement='top'>
+          <HeartOutlined
+            className='heart-outlined'
+            onClick={handleAddToFavourites}
+          />
+        </Tooltip>
+      )}
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <MuiAlert onClose={handleClose} severity='info' variant='filled'>
+          You need to be signed in to add items to favourites
+        </MuiAlert>
+      </Snackbar>
+    </>
   );
 };
 
--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
+import { clearCart } from '../../redux/cart/cart.actions';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import CustomButton from '../custom-button/custom-button.component';
 import { ReactComponent as DoubleArrow } from '../../assets/double-arrow-right.svg';
 
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({ price, history, currentUser, clearCart }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_WCNEw15nsFQIhLBa1t7b1C9W00kQiGJ8zf';
+
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [alertMsg, setAlertMsg] = useState({
+    type: 'info',
+    content: '',
+  });
+
+  const handleUserNotSignedIn = () => {
+    setAlertMsg({
+      type: 'info',
+      content: 'You need to be signed in to make purchases',
+    });
+    setAlertOpen(true);
+  };
+
+  const handleClose = (_, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setAlertOpen(false);
+  };
 
   const onToken = (token) => {
     axios({
@@ -19,36 +48,69 @@ const StripeCheckoutButton = ({ price }) => {
       },
     })
       .then((res) => {
-        alert('Payment successful');
+        clearCart();
+        history.push('/');
       })
       .catch((error) => {
         console.log('Payment error: ' + JSON.parse(error));
-        alert(
-          'There was an issue with your payment. Please make sure you used the provided credit card.'
-        );
+        clearCart();
+        history.push('/');
       });
   };
 
   return (
-    <StripeCheckout
-      label='Pay Now'
-      name='Padma Co.'
-      billingAddress
-      shippingAddress
-      image='https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/lotus.svg?alt=media&token=05a38c2f-ef7a-4405-bf6b-80d33ef29e49'
-      description={`Your total is € ${price}`}
-      currency='EUR'
-      amount={priceForStripe}
-      panelLabel='Pay Now'
-      token={onToken}
-      stripeKey={publishableKey}
-    >
-      <CustomButton>
-        PAY NOW
-        <DoubleArrow classname='double-arrow' />
-      </CustomButton>
-    </StripeCheckout>
+    <>
+      {currentUser ? (
+        <StripeCheckout
+          label='Pay Now'
+          name='Padma Co.'
+          billingAddress
+          shippingAddress
+          image='https://firebasestorage.googleapis.com/v0/b/padma-db.appspot.com/o/lotus.svg?alt=media&token=05a38c2f-ef7a-4405-bf6b-80d33ef29e49'
+          description={`Your total is € ${price}`}
+          currency='EUR'
+          amount={priceForStripe}
+          panelLabel='Pay Now'
+          token={onToken}
+          stripeKey={publishableKey}
+        >
+          <CustomButton>
+            PAY NOW
+            <DoubleArrow className='double-arrow' />
+          </CustomButton>
+        </StripeCheckout>
+      ) : (
+        <CustomButton onClick={handleUserNotSignedIn}>
+          PAY NOW
+          <DoubleArrow className='double-arrow' />
+        </CustomButton>
+      )}
+      <Snackbar
+        open={alertOpen}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <MuiAlert
+          onClose={handleClose}
+          severity={alertMsg.type}
+          variant='filled'
+        >
+          {alertMsg.content}
+        </MuiAlert>
+      </Snackbar>
+    </>
   );
 };
 
-export default StripeCheckoutButton;
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  clearCart: () => dispatch(clearCart()),
+});
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(StripeCheckoutButton)
+);
