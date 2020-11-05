a77dbf3dba6908a8f20a680827c58b602fb56011
lucievr <lucy.vrsovska@gmail.com>
2020-11-05 17:19:54 +0100

    Implement hamburger menu for mobile screens

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux'; // HOC
+import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import AppBar from '@material-ui/core/AppBar';
@@ -8,47 +8,78 @@ import Toolbar from '@material-ui/core/Toolbar';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import MobileMenu from '../mobile-menu/mobile-menu.component';
 import { selectCurrentUser } from '../../redux/user/user.selectors.js';
 import { selectWindowWidth } from '../../redux/app/app.selectors';
 import { signOutStart } from '../../redux/user/user.actions';
 
 import { ReactComponent as Logo } from '../../assets/lotus.svg';
+import { ReactComponent as MenuIcon } from '../../assets/menu.svg';
 import './header.styles.scss';
 
 const Header = ({ currentUser, signOutStart, windowWidth }) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   console.log(windowWidth, 'width');
 
   return (
-  <>
-    <AppBar position='fixed' className='app-bar'>
-      <Toolbar className='toolbar'>
-        <Link className='logo-container' to='/'>
-          <Logo className='logo' />
-        </Link>
-          <div className='options'>
-          <Link className='option' to='/shop'>
-            SHOP
-          </Link>
-          <Link className='option' to='/shop'>
-            CONTACT
-          </Link>
-          {currentUser ? (
-            <div className='option' onClick={signOutStart}>
-              SIGN OUT
-            </div>
+    <>
+      <AppBar position='fixed' className='app-bar'>
+        <Toolbar className='toolbar'>
+          {windowWidth > 500 ? (
+            <>
+              <Link className='logo-container' to='/'>
+                <Logo className='logo' />
+              </Link>
+              <div className='options'>
+                <Link className='option' to='/shop'>
+                  SHOP
+                </Link>
+                <Link className='option' to='/shop'>
+                  CONTACT
+                </Link>
+                {currentUser ? (
+                  <div className='option' onClick={signOutStart}>
+                    SIGN OUT
+                  </div>
+                ) : (
+                  <Link className='option' to='/signin'>
+                    SIGN IN
+                  </Link>
+                )}
+              </div>
+            </>
           ) : (
-            <Link className='option' to='/signin'>
-              SIGN IN
-            </Link>
+            <>
+              <div className='menu-icon'>
+                <MenuIcon
+                  aria-controls='mobile-menu'
+                  aria-haspopup='true'
+                  onClick={handleClick}
+                />
+              </div>
+              <MobileMenu anchorEl={anchorEl} onClose={handleClose} />
+              <Link className='logo-container' to='/'>
+                <Logo className='logo' />
+              </Link>
+            </>
           )}
-          </div>
           <CartIcon />
-        <CartDropdown />
-      </Toolbar>
-    </AppBar>
-    <Toolbar id='back-to-top-anchor' />
-  </>
-)};
+          <CartDropdown />
+        </Toolbar>
+      </AppBar>
+      <Toolbar id='back-to-top-anchor' />
+    </>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
---
+++ client/src/components/mobile-menu/mobile-menu.component.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { Link } from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import { signOutStart } from '../../redux/user/user.actions';
+import { selectCurrentUser } from '../../redux/user/user.selectors.js';
+
+import './mobile-menu.styles.scss';
+
+const MobileMenu = ({ anchorEl, onClose, currentUser, signOutStart }) => {
+  return (
+    <Menu
+      className='mobile-menu'
+      elevation={0}
+      getContentAnchorEl={null}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      id='mobile-menu'
+      anchorEl={anchorEl}
+      keepMounted
+      open={Boolean(anchorEl)}
+      onClose={onClose}
+    >
+      <MenuItem>
+        <Link className='option' to='/' onClick={onClose}>
+          HOME
+        </Link>
+      </MenuItem>
+      <MenuItem>
+        <Link className='option' to='/shop' onClick={onClose}>
+          SHOP
+        </Link>
+      </MenuItem>
+      <MenuItem>
+        <Link className='option' to='/shop' onClick={onClose}>
+          CONTACT
+        </Link>
+      </MenuItem>
+      {currentUser ? (
+        <MenuItem>
+          <div
+            className='option'
+            onClick={() => {
+              signOutStart();
+              onClose();
+            }}
+          >
+            SIGN OUT
+          </div>
+        </MenuItem>
+      ) : (
+        <MenuItem>
+          <Link className='option' to='/signin' onClick={onClose}>
+            SIGN IN
+          </Link>
+        </MenuItem>
+      )}
+    </Menu>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  signOutStart: () => dispatch(signOutStart()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MobileMenu);
